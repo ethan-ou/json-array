4bb80ac4b77eaa66e4492c8ef21763d1cb3e7a72
ethan-ou <ethantim@gmail.com>
2020-07-03 01:24:40 +1000

    Refactor to cleaner code

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,13 +1,58 @@
-import JSONArray from '../src';
+import JSONArray, { convertArrayIndexKeys } from '../src';
 
 describe('JSONArray', () => {
-  it('converts JSON arrays to Javascript Arrays', () => {
-    const originalArray = [
+  it('converts JSON Top Level arrays to Javascript Arrays', () => {
+    const originalArray = {
+      '0': {
+        type: 'item',
+        name: 'Picnic Basket',
+        suppliers: {
+          '0': {
+            country: 'USA',
+            stores: {
+              '0': {
+                name: 'General Store',
+                price: 24.25,
+              },
+              '1': {
+                name: 'Local Store',
+                price: 26.99,
+              },
+            },
+          },
+          '1': { country: 'Germany', price: 19.5 },
+          '2': { country: 'Sweden', price: 23.2 },
+        },
+      },
+      '1': {
+        type: 'item',
+        name: 'T-Shirt',
+        suppliers: {
+          '0': { country: 'USA', price: 45.95 },
+          '1': { country: 'Germany', price: 49.5 },
+          '2': { country: 'Sweden', price: 37.75 },
+        },
+      },
+    };
+
+    const targetArray = [
       {
         type: 'item',
         name: 'Picnic Basket',
         suppliers: [
-          { country: 'USA', price: 20.0 },
+          {
+            country: 'USA',
+            stores: [
+              {
+                name: 'General Store',
+                price: 24.25,
+              },
+              {
+                name: 'Local Store',
+                price: 26.99,
+              },
+            ],
+          },
           { country: 'Germany', price: 19.5 },
           { country: 'Sweden', price: 23.2 },
         ],
@@ -24,6 +69,101 @@ describe('JSONArray', () => {
     ];
 
     const stringifiedArray = JSON.parse(JSON.stringify(originalArray));
+    expect(JSONArray(stringifiedArray)).toEqual(targetArray);
+  });
+
+  it('converts JSON Objects to Javascript Arrays', () => {
+    const originalArray = {
+      type: 'item',
+      name: 'T-Shirt',
+      suppliers: [
+        { country: 'USA', price: 45.95 },
+        { country: 'Germany', price: 49.5 },
+        { country: 'Sweden', price: 37.75 },
+      ],
+    };
+    const stringifiedArray = JSON.parse(JSON.stringify(originalArray));
     expect(JSONArray(stringifiedArray)).toEqual(originalArray);
+  });
+
+  it('avoids mutation of original object', () => {
+    const originalArray = {
+      '0': {
+        type: 'item',
+        name: 'Picnic Basket',
+        suppliers: {
+          '0': {
+            country: 'USA',
+            stores: {
+              '0': {
+                name: 'General Store',
+                price: 24.25,
+              },
+              '1': {
+                name: 'Local Store',
+                price: 26.99,
+              },
+            },
+          },
+          '1': { country: 'Germany', price: 19.5 },
+          '2': { country: 'Sweden', price: 23.2 },
+        },
+      },
+    };
+
+    const duplicatedArray = {
+      '0': {
+        type: 'item',
+        name: 'Picnic Basket',
+        suppliers: {
+          '0': {
+            country: 'USA',
+            stores: {
+              '0': {
+                name: 'General Store',
+                price: 24.25,
+              },
+              '1': {
+                name: 'Local Store',
+                price: 26.99,
+              },
+            },
+          },
+          '1': { country: 'Germany', price: 19.5 },
+          '2': { country: 'Sweden', price: 23.2 },
+        },
+      },
+    };
+
+    JSONArray(originalArray);
+    expect(originalArray).toEqual(duplicatedArray);
+  });
+});
+
+describe('testForArrayIndexKeys', () => {
+  it('works', () => {
+    const originalArray = {
+      '0': {
+        name: 'General Store',
+        price: 24.25,
+      },
+      '1': {
+        name: 'Local Store',
+        price: 26.99,
+      },
+    };
+
+    const expectedArray = [
+      {
+        name: 'General Store',
+        price: 24.25,
+      },
+      {
+        name: 'Local Store',
+        price: 26.99,
+      },
+    ];
+
+    expect(convertArrayIndexKeys(originalArray)).toEqual(expectedArray);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,76 @@
-export default function convertJSONArray(obj) {
-  let newObj = Object.assign({}, obj);
+export type JSONObject = Record<string, unknown> | Array<unknown>;
 
-  const checkObject = (obj) => {
-    const keys = Object.keys(obj)
-      .map((currKey) => parseInt(currKey, 10))
-      .sort((a, b) => a - b);
-    const numberCheck = keys.every((currItem) => Number.isInteger(currItem));
-    const arrayIndexCheck = keys.every((currItem, idx) => currItem === idx);
-    if (keys.length === 0 || !numberCheck || !arrayIndexCheck) {
-      return obj;
-    }
-    return Object.values(obj);
-  };
-
-  const checkAllKeys = (obj: ObjectConstructor | any[]) => {
-    if (Array.isArray(obj)) {
-      for (let i = 0; i < obj.length; i++) {
-        if (typeof obj[i] === 'object') {
-          obj[i] = checkObject(obj[i]);
-          checkAllKeys(obj[i]);
-        }
-      }
-    } else {
-      for (let key in obj) {
-        if (typeof obj[key] === 'object') {
-          obj[key] = checkObject(obj[key]);
-          checkAllKeys(obj[key]);
-        }
-      }
-    }
-  };
-
-  newObj = checkObject(obj);
-  checkAllKeys(newObj);
-
-  return newObj;
+/**
+ * Converts a JSON object with strings for array indicies
+ * into a Javascript array.
+ * @param object JSON object.
+ */
+export default function JSONArray(object: JSONObject): JSONObject {
+  //Check the object itself once before checking all of its keys.
+  return checkAllKeys(convertArrayIndexKeys({ ...object }));
 }
+
+/**
+ * Checks for keys in an object that are possibly array indicies.
+ * When a match is found, returns a new array with the items of the
+ * object.
+ * @param object Input object
+ * @returns {boolean} Returns true if object is a possible array.
+ */
+export const convertArrayIndexKeys = (object: JSONObject): JSONObject => {
+  const keyIdx: number[] = [];
+  for (const key in object) {
+    const numberConversion = Number.parseInt(key, 10);
+    if (!Number.isInteger(numberConversion) || numberConversion < 0) {
+      return object;
+    }
+
+    keyIdx.push(numberConversion);
+  }
+
+  if (keyIdx.length === 0) {
+    return object;
+  }
+
+  // Sort in case keys are out of order.
+  const sortedKeyIdx = keyIdx.sort((a, b) => a - b);
+
+  for (let i = 0; i < keyIdx.length; i++) {
+    if (sortedKeyIdx[i] !== i) {
+      return object;
+    }
+  }
+
+  return Object.values(object);
+};
+
+/**
+ * Recursively checks through an object for indicies that can
+ * be converted into an array.
+ * @param object
+ */
+const checkAllKeys = (object: JSONObject): JSONObject => {
+  const checkArrayKeys = (object: Array<unknown>) =>
+    object.map((item) =>
+      typeof item === 'object'
+        ? checkAllKeys(convertArrayIndexKeys(item as JSONObject))
+        : item,
+    );
+
+  const checkObjectKeys = (object: Record<string, unknown>) => {
+    const result = Object.assign({}, object);
+    for (const key in result) {
+      if (typeof object[key] === 'object') {
+        result[key] = checkAllKeys(
+          convertArrayIndexKeys(result[key] as JSONObject),
+        );
+      }
+    }
+
+    return result;
+  };
+
+  return Array.isArray(object)
+    ? checkArrayKeys(object)
+    : checkObjectKeys(object);
+};
